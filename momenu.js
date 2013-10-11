5128f7772701f6e5db0282df217ca5aa5980d470
Daryn St. Pierre <daryn@bloqmail.com>
2013-10-11 10:10:42 -0400

    Update momenu.js
    Little cleanup items. Converted some reused things into functions. FYI: haven't tested in the wild yet. Use at your own risk and report back w/ pull requests.

--- momenu.js
+++ momenu.js
@@ -1,15 +1,21 @@
 (function($) {
 	/*
-	* Mo' Menu -- v1.3.6
+	* Mo' Menu -- v1.3.8
 	* Mo' Money, Mo' Menu
-	* 
+	*
 	* (c) 2012, Daryn St. Pierre http://bloqhead.com
 	* Released under the WTFPL License
 	* http://sam.zoy.org/wtfpl/
 	*
 	* Creation Date: Tue July 24, 2012
+	*
+	* @todo:
+	* - Make use of CSS3 animations instead of jQuery
+	* - Cleanup some items and update methods
+	* - Test more rigorously
+	* - More themes and/or better theming methods
 	*/
-	
+
 	$.fn.extend({
 		moMenu: function(options) {
 			var defaults = {
@@ -24,12 +30,12 @@
 				theme: '' // light, default, your own
 			};
 			var options = $.extend(defaults, options);
-			
+
 			// --------------------------------
 			// MoMenu
 			// --------------------------------
 			return this.each(function() {
-				
+
 				// ---------------------------------------------------------
 				// Core Variables
 				// ---------------------------------------------------------
@@ -37,64 +43,70 @@
 				var obj = $(this);
 				var cid = o.container.replace('#', '').replace('.', '');
 				var mobileWidth = 480;
-				
+
 				// ---------------------------------------------------------
 				// Core Functions
 				// ---------------------------------------------------------
-				
-				// automatically prepend #mobileMenu to body if desired
+
+				// Find window dimensions
+				function docDimensions() {
+					var bHeight = $(window).height();
+					var bWidth = $(window).width();
+				}
+
+				// Automatically prepend #mobileMenu to body if desired
 				if(o.auto == true) {
 					$('body').prepend('<div id="' + cid + '" class="' + cid +'" aria-hidden="true"></div>');
 				}
-				
-				// themes
+
+				// Theming
 				if(o.theme != '') {
 					$(o.container).addClass('moMenu-' + o.theme);
 				}
-				
-				// clone the menu ul from the navigation container
+
+				// Clone the menu ul from the navigation container
 				if(o.nocontainer == false) {
 					$('ul', obj).clone().appendTo(o.container).hide();
 				} else {
 					$(obj).clone().appendTo(o.container);
 				}
-				
-				// strip existing classes and id from cloned menu
-				// cleans the cloned menu up so that overlapping styles don't occur
+
+				// Strip existing classes and id from cloned menu
+				// Cleans the cloned menu up so that overlapping styles don't occur
 				$('ul', o.container).attr('class','').attr('id','');
-				
+
 				// z-index (added 10-17-2012)
 				if(o.zindex != '') {
 					$(o.container).css('z-index',o.zindex);
 				}
-				
+
 				// ---------------------------------------------------------
 				// Menu Type -- Options: 'slide'
 				// ---------------------------------------------------------
-				
-				// type (added: 10/24/12)
+
+				// Menu Type (added: 10/24/12)
 				if(o.type == 'slide') {
-					// mobile menu toggle button
+					
+					// Mobile menu toggle button
 					if(o.theme != '') {
 						$('body').prepend('<span id="toggleMobileMenuSlide-'+ o.theme +'"><a id="toggleMobileMenu">Menu</a></span>');
 					} else {
 						$('body').prepend('<span id="toggleMobileMenuSlide"><a id="toggleMobileMenu">Menu</a></span>');
 					}
-					
-					var bHeight = $(window).height();
-					var bWidth = $(window).width();
+
+					docDimensions();
 					$(o.container).addClass('moMenu-slideOut');
 					$(o.container).width(bWidth - 20 + 'px');
 					$('ul', o.container).show();
 					$(o.container)
 						.css('position','fixed')
 						.css('left','-' + bWidth + 'px');
-					
+
 					// toggle button
 					$('a#toggleMobileMenu').css('z-index',o.zindex + '1');
 					$(o.container).css('top','50px');
 					$(o.container + 'ul').css('display','inline');
-					
+
 					// toggle the slide out animation
 					$('a#toggleMobileMenu').toggle(
 						function() {
@@ -106,20 +118,19 @@
 							$(this).removeClass('active');
 						}
 					);
-					
+
 					// determine the menu dimensions based on browser dimensions
 					$(window).resize(function() {
-						var bHeight = $(window).height();
-						var bWidth = $(window).width();
+						docDimensions();
 						$(o.container).addClass('moMenu-slideOut');
 						$(o.container).width(bWidth).height(bHeight);
 					});
-					
+
 				} else {
-					
+
 					// mobile menu toggle button
 					$(o.container).prepend('<a id="toggleMobileMenu">Menu</a>');
-					
+
 					// toggle button
 					$('a#toggleMobileMenu').click(function() {
 						if(o.animation == '') {
@@ -130,12 +141,12 @@
 						$(this).toggleClass('active');
 					});
 				}
-				
+
 				// ---------------------------------------------------------
-				// Browser Resizing + Polling
+				// Browser Resizing
 				// ---------------------------------------------------------
-				
-				// top padding on body for initial load (added 10-19-2012)
+
+				// Top padding on body for initial load (added 10-19-2012)
 				function bodyPadding() {
 					var initialWidth = $(window).width();
 					if(o.padding == '' && initialWidth < mobileWidth) {
@@ -146,14 +157,14 @@
 						$('body').css('padding-top','');
 					}
 				}
-				
+
 				bodyPadding();
-				
+
 				// top padding on body when resized (added 10-19-2012)
 				$(window).resize(function() {
 					bodyPadding();
 				});
-				
+
 			});
 		}
 	});
